2f856014cc93887587b8ce686bf5eac983e2b8f8
Serdar Mengütay <serdarmngty03@gmail.com>
2022-11-24 00:42:39 +0300

    Picking randomly users from Rest Api with using fetch and adding to the UI

---
+++ Random User Project/randomUser.js
@@ -0,0 +1,25 @@
+const details = document.querySelector(".details");
+const imgContainer = document.querySelector(".img-container");
+const getUserBtn = document.getElementById("get-user-btn");
+const url = "https://random-data-api.com/api/v2/users?response_type=json";
+
+const getUser = () => { 
+    fetch(url)
+    .then(resp => resp.json())
+    .then((data) => {
+        imgContainer.innerHTML = `<img src = ${data.avatar}>`;  
+        details.innerHTML = `
+        <h2>${data.first_name} ${data.last_name}</h2>
+        <h3>${data.employment.title}</h3>
+        <h4><i class = "fa fa-solid fa-location-dot"></i> ${data.address.city}</h4>`;    
+
+
+            let randomColor = 
+            "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
+            document.documentElement.style.setProperty("--theme-color" , randomColor);
+        
+        });
+};
+
+window.addEventListener("load", getUser);
+getUserBtn.addEventListener("click", getUser);
